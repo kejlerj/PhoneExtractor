62602cb1b61a8f235893b07a326b61bac5289747
kejlerj <kejlerj@gmail.com>
2021-09-24 16:48:39 +0200

    Add phone extractor route

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require("express")();
+const axios = require("axios");
+require("dotenv").config();
+
+const getPhoneNumber = async (company, address, siren) => {
+  const re = /(?:(?:\+|00)33|\(\+33\)|0)\s*[1-9](?:\s*\d{2}){4}/m;
+
+  return await axios
+    .get(`${process.env.GOOGLE_SEARCH} ${company} ${address} ${siren}`)
+    .then((res) => {
+      const phone = res.data.match(re);
+      return phone?.length ? phone[0] : undefined;
+    })
+    .catch((err) => console.log(err));
+};
+
+app.get("/", async (req, res) => {
+  const company = req.query.company;
+  const address = req.query.address ?? "";
+  const siren = req.query.siren ?? "";
+
+  if (!company) return res.status(400).send("You should specify a company");
+
+  let phone = await getPhoneNumber(company, address, siren);
+  phone = phone?.replaceAll(" ", "");
+  res.json({ phone }).send();
+});
+
+// Error handling
+app.use((err, req, res) => {
+  console.error("Error", err);
+  res.status(500).send();
+});
+
+if (process.env.NODE_ENV !== "test") {
+  // Launch the server
+  app.listen(process.env.PORT, () =>
+    console.log(`App listening at http://localhost:${process.env.PORT}`)
+  );
+}
+
+module.exports = app;
